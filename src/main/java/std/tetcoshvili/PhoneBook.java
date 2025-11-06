40a0e875b1ea640442a5889feebb8d5fe2fab851
TV <v.tetcoshvili@petrovich.tech>
2025-11-07 02:20:28 +0300

    Stub and tests for add

--- src/main/java/std/tetcoshvili/PhoneBook.java
+++ src/main/java/std/tetcoshvili/PhoneBook.java
@@ -1,4 +1,10 @@
 package std.tetcoshvili;
 
 public class PhoneBook {
+
+    // Создать пустой метод-заглушку add в классе PhoneBook,
+    // который только возвращает 0.
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/std/tetcoshvili/PhoneBookTest.java
+++ src/test/java/std/tetcoshvili/PhoneBookTest.java
@@ -14,4 +14,44 @@ class PhoneBookTest {
     }
 
     // Тесты будут добавляться тут
+
+    // Создать тест или тесты на метод add в классе тестов PhoneBookTest.
+
+    @Test
+    void testAdd_firstContact() {
+
+        String name = "Vlad";
+        String number = "8-921-099-83-45";
+
+        int count = phoneBook.add(name, number);
+
+        assertEquals(1, count, "Add 1");
+    }
+
+    @Test
+    void testAdd_secondContact() {
+
+        phoneBook.add("Vlad", "8-921-099-83-45");
+
+        String name = "SvetaBolshie";
+        String number = "8-911-101-12-11";
+
+        int count = phoneBook.add(name, number);
+
+        assertEquals(2, count, "Add 2");
+    }
+
+    @Test
+    void testAdd_duplicateName() {
+        phoneBook.add("Vlad", "123-456");
+        phoneBook.add("SvetaBolshie", "8-911-101-12-11");
+
+        String duplicateName = "Vlad";
+        String newNumber = "8-911-999-99-99"; // Меняем номер
+
+        int count = phoneBook.add(duplicateName, newNumber);
+
+        assertEquals(2, count, "Add Double");
+    }
+
 }
